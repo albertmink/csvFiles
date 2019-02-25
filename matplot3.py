c100b564756fe8e96b0d36001753f3c67447f75a
albert mink <albert.mink@kit.edu>
2019-02-25 16:37:17 +0100

    add plot routine for csv data

---
+++ matplot3.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+print(os.getcwd())
+
+parser = argparse.ArgumentParser(description='plot a given case with two different solutions')
+
+parser.add_argument("--outputfile", help="file to write data", default="test.png",  metavar="output")
+parser.add_argument("--csvFile", help="path to csv data", nargs='*',)
+
+args = parser.parse_args()
+
+print(args)
+
+for csv in args.csvFile:
+  if( not(os.path.exists(csv)) ):
+    print("! file are not available: "+csv )
+
+title=args.outputfile
+
+mu_eff = 4.4
+
+for csv in args.csvFile:
+  if( (os.path.exists(csv)) ):
+    data = np.genfromtxt( csv, delimiter=',', comments='#', dtype=float, usecols=(0,1,2,3) );
+    plt.plot(data[:,0],data[:,1], '--', label=str(csv));
+#    plt.plot(data[:,0],7*np.exp(-16*data[:,0])+data[:,1], '+', label=str(csv)+"uncollided");
+    #plt.plot(data[:,0], 3*np.exp(-mu_eff*data[:,0]), label="uncollided exp(-"+str(mu_eff)+"*r)")
+    #dataLena = np.genfromtxt( "case19_lena.csv", delimiter=',', comments='#', dtype=float );
+    #plt.plot(dataLena[:,0],12000000*dataLena[:,1], label='lena 2*10^7')
+    print(csv)
+    title = os.path.basename(csv)
+
+
+
+plt.yscale('log');
+plt.ylim([10e-5,7]);
+#plt.xticks(np.arange(0,1,1/mu_eff),np.arange(0,mu_eff));
+plt.legend(loc='upper right');
+plt.title(title);
+plt.savefig(args.outputfile,dpi=240);
+print("save figure to "+args.outputfile)
+plt.close()
