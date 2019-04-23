4a098d62e6b6a9facee434c45dcc8ad6e4e9dd95
albert mink <albert.mink@kit.edu>
2019-04-23 14:11:02 +0200

    mv to pandas dataframe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import getXY
 import numpy as np
 from numpy import linalg as LA
+import pandas as pd
 
 import matplotlib
 matplotlib.use('Agg')
@@ -10,46 +11,51 @@ import matplotlib.pyplot as plt
 'get me all norm gerichted'
 absErrMcHardy = []
 absErrMcHardyRel = []
+absErrMinkN50 = []
+absErrMinkRelN50 = []
 absErrMink = []
 absErrMinkRel = []
 absErrMinkT1 = []
 absErrMinkRelT1 = []
+
+dfMacro = pd.DataFrame(index=np.linspace(0,1,101))
+dfMeso = pd.DataFrame(index=np.linspace(0,1,101))
+erMacro = pd.DataFrame(index=np.arange(1,36))
+erMeso = pd.DataFrame(index=np.arange(1,36))
+
+
+
 for i in range(1,36):
-    absErrMinkT1.append(getXY.getNorm('minkTauEins/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' ))
-    absErrMinkRelT1.append(getXY.getNormRel('minkTauEins/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' ))
-    absErrMink.append(getXY.getNorm('mink/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' ))
-    absErrMinkRel.append(getXY.getNormRel('mink/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' ))
-    absErrMcHardy.append(getXY.getNorm('mcHardyRK/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' ))
-    absErrMcHardyRel.append(getXY.getNormRel('mcHardyRK/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' ))
-    dataMinkT1 = getXY.getDataMink('minkTauEins/100case'+str(i)+'.csv')
-    plt.plot(dataMinkT1[:,0],dataMinkT1[:,1], 'c-', label='macro tau=1')
-    dataMink = getXY.getDataMink('mink/100case'+str(i)+'.csv')
-    plt.plot(dataMink[:,0],dataMink[:,1], 'm-', label='macro')
-    dataLena = getXY.getDataLena('lena/lena'+str(i)+'.csv')
-    plt.plot(dataLena[:,0], dataLena[:,1], 'y-', label='MC')
-    plt.legend()
-    plt.yscale('log');
+    # macro 
+    erMacro.at[i,'abs50'] = getXY.getNorm('mink/50case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    erMacro.at[i,'abs100'] = getXY.getNorm('mink/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    erMacro.at[i,'rel50'] = getXY.getNormRel('mink/50case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    erMacro.at[i,'rel100'] = getXY.getNormRel('mink/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    erMacro.at[i,'absT1_100'] = getXY.getNorm('minkTauEins/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    erMacro.at[i,'relT1_100'] = getXY.getNormRel('minkTauEins/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    dfMacro['50'] = pd.Series(getXY.getDataMink('mink/50case'+str(i)+'.csv')[:,1], index=dfMacro.index)
+    dfMacro['100'] = pd.Series(getXY.getDataMink('mink/100case'+str(i)+'.csv')[:,1], index=dfMacro.index)
+    #dfMacro['150'] = pd.Series(getXY.getDataMink('mink/150case'+str(i)+'.csv')[:,1], index=dfMacro.index)
+    dfMacro['MC'] = pd.Series(getXY.getDataLena('lena/lena'+str(i)+'.csv')[:,1], index=dfMacro.index)
+    #plot single cases
+    dfMacro.plot()
+    plt.yscale('log')
     plt.savefig('plots/macroMC'+str(i)+'.png')
     plt.close()
-    dataMcHardy = getXY.getDataMcHardy('mcHardyRK/100case'+str(i)+'.csv')
-    plt.plot(dataMcHardy[:,0],dataMcHardy[:,1], 'g-', label='meso')
-    plt.plot(dataLena[:,0], dataLena[:,1], 'y-', label='MC')
-    plt.legend()
-    plt.yscale('log');
+    # meso
+    erMeso.at[i,'absMeso'] = getXY.getNorm('mcHardyRK/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    erMeso.at[i,'relMeso'] = getXY.getNormRel('mcHardyRK/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    #plot single cases
+    dfMeso['100'] = pd.Series(getXY.getDataMcHardy('mcHardyRK/100case'+str(i)+'.csv')[:,1], index=dfMacro.index)
+    dfMeso.plot()
+    plt.yscale('log')
     plt.savefig('plots/mesoMC'+str(i)+'.png')
     plt.close()
 
-#print(absErrMink)
-#print(absErrMinkRel)
 
+tmp = erMacro.join(erMeso)
+print(tmp)
+tmp.plot()
 plt.yscale('log');
-#plt.ylim([10e-4,10]);
-plt.plot(absErrMinkT1, 'c--', label='minkT1');
-plt.plot(absErrMinkRelT1, 'c.', label='minkRelT1');
-plt.plot(absErrMink, 'm--', label='mink');
-plt.plot(absErrMinkRel, 'm.', label='minkRel');
-plt.plot(absErrMcHardy, 'g--', label='mcHardy');
-plt.plot(absErrMcHardyRel, 'g.', label='mcHardyRel');
-plt.legend(loc='center left');
 plt.savefig('absErrMink.png',dpi=240);
 plt.close()
