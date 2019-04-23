b75991767fd7dc8443c1a4c6fd05902654f21ef5
albert mink <albert.mink@kit.edu>
2019-04-23 14:36:17 +0200

    write csv

--- main.py
+++ main.py
@@ -8,22 +8,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-'get me all norm gerichted'
-absErrMcHardy = []
-absErrMcHardyRel = []
-absErrMinkN50 = []
-absErrMinkRelN50 = []
-absErrMink = []
-absErrMinkRel = []
-absErrMinkT1 = []
-absErrMinkRelT1 = []
-
-dfMacro = pd.DataFrame(index=np.linspace(0,1,101))
-dfMeso = pd.DataFrame(index=np.linspace(0,1,101))
 erMacro = pd.DataFrame(index=np.arange(1,36))
 erMeso = pd.DataFrame(index=np.arange(1,36))
-
-
 
 for i in range(1,36):
     # macro 
@@ -33,6 +19,7 @@ for i in range(1,36):
     erMacro.at[i,'rel100'] = getXY.getNormRel('mink/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
     erMacro.at[i,'absT1_100'] = getXY.getNorm('minkTauEins/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
     erMacro.at[i,'relT1_100'] = getXY.getNormRel('minkTauEins/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
+    dfMacro = pd.DataFrame(index=np.linspace(0,1,101))
     dfMacro['50'] = pd.Series(getXY.getDataMink('mink/50case'+str(i)+'.csv')[:,1], index=dfMacro.index)
     dfMacro['100'] = pd.Series(getXY.getDataMink('mink/100case'+str(i)+'.csv')[:,1], index=dfMacro.index)
     #dfMacro['150'] = pd.Series(getXY.getDataMink('mink/150case'+str(i)+'.csv')[:,1], index=dfMacro.index)
@@ -45,8 +32,9 @@ for i in range(1,36):
     # meso
     erMeso.at[i,'absMeso'] = getXY.getNorm('mcHardyRK/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
     erMeso.at[i,'relMeso'] = getXY.getNormRel('mcHardyRK/100case'+str(i)+'.csv', 'lena/lena'+str(i)+'.csv' )
-    #plot single cases
+    dfMeso = pd.DataFrame(index=np.linspace(0,1,101))
     dfMeso['100'] = pd.Series(getXY.getDataMcHardy('mcHardyRK/100case'+str(i)+'.csv')[:,1], index=dfMacro.index)
+    #plot single cases
     dfMeso.plot()
     plt.yscale('log')
     plt.savefig('plots/mesoMC'+str(i)+'.png')
@@ -55,7 +43,13 @@ for i in range(1,36):
 
 tmp = erMacro.join(erMeso)
 print(tmp)
-tmp.plot()
+tmp.to_csv('err.csv', float_format='%8.6e')
+
+tmp[['abs50','abs100','absT1_100','absMeso']].plot()
 plt.yscale('log');
-plt.savefig('absErrMink.png',dpi=240);
+plt.savefig('absErr.png',dpi=240);
+plt.close()
+tmp[['rel50','rel100','relT1_100','relMeso']].plot()
+plt.yscale('log');
+plt.savefig('relErr.png',dpi=240);
 plt.close()
