ec8545aa0f2609b52b29f75d16c9c6fbe1e40ea8
albert mink <albert.mink@kit.edu>
2019-05-24 10:31:59 +0200

    get total energy

--- getXY.py
+++ getXY.py
@@ -12,6 +12,14 @@ def getDataMcHardy( caseFile, line ):
 def getDataLena( caseFile, line ):
     return np.genfromtxt( caseFile, delimiter=',', comments='#', dtype=float, usecols=(0,7+line) );
 
+def getTotalEnergy( caseLena, line, indexAwayFromBoundary=0 ):
+    dataLena = getDataLena( caseLena, line )
+    index_to_keep = np.arange(1+indexAwayFromBoundary,99,1); # to slice array to voxel inside geometry
+    if len(index_to_keep) < 1:
+      return 1
+    else:
+      return 1./len(index_to_keep) * np.sum(dataLena[:,1][index_to_keep]) ;
+
 'compute l2 norm from voxel inside the geometry'
 def getNorm( caseNb1, dataLena, line, indexAwayFromBoundary=0 ):
     dM = getDataMink(caseNb1, line);
