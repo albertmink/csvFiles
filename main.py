83b6b25a25c8fdc089e4f48a3660ff24ed4ffe7a
albert mink <albert.mink@kit.edu>
2019-06-05 09:04:16 +0200

    adopt to new directory

--- main.py
+++ main.py
@@ -16,28 +16,30 @@ energy1 = []
 energy2 = []
 energy3 = []
 
+setUp = 'infiniteBeam/'
+
 for i in range(1,36):
     mfp = dfCase.loc[i].mfp
     indexAwayFromBoundary = 1
     # mask boundary region about a mfp
     #indexAwayFromBoundary = int(round(mfp,2)*100) *1
     #print(indexAwayFromBoundary)
-    dataLena1 = getXY.getDataLena('lena/lena'+str(i)+'.csv', 1)
-    dataLena2 = getXY.getDataLena('lena/lena'+str(i)+'.csv', 2)
-    dataLena3 = getXY.getDataLena('lena/lena'+str(i)+'.csv', 3)
+    dataLena1 = getXY.getDataLena(setUp+'lena/lena'+str(i)+'.csv', 1)
+    dataLena2 = getXY.getDataLena(setUp+'lena/lena'+str(i)+'.csv', 2)
+    dataLena3 = getXY.getDataLena(setUp+'lena/lena'+str(i)+'.csv', 3)
     # macro 
-    erMacro.at[i,'absMa1'] = getXY.getNorm4('mink3/50case'+str(i)+'.csv', dataLena1, 1, indexAwayFromBoundary)
-    erMacro.at[i,'absMa2'] = getXY.getNorm4('mink3/50case'+str(i)+'.csv', dataLena2, 2, indexAwayFromBoundary)
-    erMacro.at[i,'absMa3'] = getXY.getNorm4('mink3/50case'+str(i)+'.csv', dataLena3, 3, indexAwayFromBoundary)
+    erMacro.at[i,'absMa1'] = getXY.getNorm4(setUp+'mink/50case'+str(i)+'.csv', dataLena1, 1, indexAwayFromBoundary)
+    erMacro.at[i,'absMa2'] = getXY.getNorm4(setUp+'mink/50case'+str(i)+'.csv', dataLena2, 2, indexAwayFromBoundary)
+    erMacro.at[i,'absMa3'] = getXY.getNorm4(setUp+'mink/50case'+str(i)+'.csv', dataLena3, 3, indexAwayFromBoundary)
 
     # meso
-    erMeso.at[i,'absMe1'] = getXY.getNorm('mcHardyRK/100case'+str(i)+'.csv', dataLena1, 1, indexAwayFromBoundary)
-    erMeso.at[i,'absMe2'] = getXY.getNorm('mcHardyRK/100case'+str(i)+'.csv', dataLena2, 2, indexAwayFromBoundary)
-    erMeso.at[i,'absMe3'] = getXY.getNorm('mcHardyRK/100case'+str(i)+'.csv', dataLena3, 3, indexAwayFromBoundary)
+    erMeso.at[i,'absMe1'] = getXY.getNorm(setUp+'mcHardyRK/100case'+str(i)+'.csv', dataLena1, 1, indexAwayFromBoundary)
+    erMeso.at[i,'absMe2'] = getXY.getNorm(setUp+'mcHardyRK/100case'+str(i)+'.csv', dataLena2, 2, indexAwayFromBoundary)
+    erMeso.at[i,'absMe3'] = getXY.getNorm(setUp+'mcHardyRK/100case'+str(i)+'.csv', dataLena3, 3, indexAwayFromBoundary)
 
-    energy1.append(getXY.getTotalEnergy('lena/lena'+str(i)+'.csv', 1))
-    energy2.append(getXY.getTotalEnergy('lena/lena'+str(i)+'.csv', 2))
-    energy3.append(getXY.getTotalEnergy('lena/lena'+str(i)+'.csv', 3))
+    energy1.append(getXY.getTotalEnergy(setUp+'lena/lena'+str(i)+'.csv', 1))
+    energy2.append(getXY.getTotalEnergy(setUp+'lena/lena'+str(i)+'.csv', 2))
+    energy3.append(getXY.getTotalEnergy(setUp+'lena/lena'+str(i)+'.csv', 3))
 
 
 tmp = erMacro.join(erMeso)
@@ -53,9 +55,9 @@ tmp['absMe3Normalized'] = tmp['absMe3']/tmp['totalEnergy3']
 
 tmp.index.names = ['case']
 print(tmp)
-tmp.to_csv('err_totalEnergy_review.csv', float_format='%8.6e')
+tmp.to_csv(setUp[:-1]+'.csv', float_format='%8.6e')
 
-tmp[['absMa1Normalized','absMe1Normalized']].plot()
-plt.yscale('log');
-plt.savefig('errorNormalized.png',dpi=240);
-plt.close()
+#tmp[['absMa1Normalized','absMe1Normalized']].plot()
+#plt.yscale('log');
+#plt.savefig('errorNormalized.png',dpi=240);
+#plt.close()
