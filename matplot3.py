9089278c4314a8aac12ad5c6e3dce2cd206253d6
albert mink <albert.mink@kit.edu>
2019-06-05 09:02:22 +0200

    add line argument to plot

--- matplot3.py
+++ matplot3.py
@@ -10,7 +10,8 @@ print(os.getcwd())
 parser = argparse.ArgumentParser(description='plot a given case with two different solutions')
 
 parser.add_argument("--outputfile", help="file to write data", default="test.png",  metavar="output")
-parser.add_argument("--csvFile", help="path to csv data", nargs='*',)
+parser.add_argument("--csvFile", help="path to csv data", nargs='*')
+parser.add_argument("--line", help="path to csv data", default=1, type=int)
 
 args = parser.parse_args()
 
@@ -26,19 +27,27 @@ mu_eff = 4.4
 
 for csv in args.csvFile:
   if( (os.path.exists(csv)) ):
-    data = np.genfromtxt( csv, delimiter=',', comments='#', dtype=float, usecols=(0,1,2,3) );
-    plt.plot(data[:,0],data[:,1], '--', label=str(csv));
+    if( "lena" in csv ) :
+      data = np.genfromtxt( csv, delimiter=',', comments='#', dtype=float, usecols=(0,7+args.line) );
+      plt.plot(data[:,0],data[:,1], '--', label=str(csv));
+    elif( "mink" in csv ):
+      data = np.genfromtxt( csv, delimiter=',', comments='#', dtype=float, usecols=(0,1,2,3) );
+      plt.plot(data[:,0],data[:,args.line]*4, '--', label=str(csv));
+    else :
+      data = np.genfromtxt( csv, delimiter=',', comments='#', dtype=float, usecols=(0,1,2,3) );
+      plt.plot(data[:,0],data[:,args.line], '--', label=str(csv));
+
 #    plt.plot(data[:,0],7*np.exp(-16*data[:,0])+data[:,1], '+', label=str(csv)+"uncollided");
     #plt.plot(data[:,0], 3*np.exp(-mu_eff*data[:,0]), label="uncollided exp(-"+str(mu_eff)+"*r)")
     #dataLena = np.genfromtxt( "case19_lena.csv", delimiter=',', comments='#', dtype=float );
     #plt.plot(dataLena[:,0],12000000*dataLena[:,1], label='lena 2*10^7')
     print(csv)
-    title = os.path.basename(csv)
+    title = "line"+str(args.line)
 
 
 
 plt.yscale('log');
-plt.ylim([10e-5,7]);
+#plt.ylim([1e-7,7]);
 #plt.xticks(np.arange(0,1,1/mu_eff),np.arange(0,mu_eff));
 plt.legend(loc='upper right');
 plt.title(title);
