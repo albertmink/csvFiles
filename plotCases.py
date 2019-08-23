cf5dc466aea7347b75ab0176d8a01f0fa3896e75
albert mink <albert.mink@kit.edu>
2019-08-23 15:34:40 +0200

    no more includes in postprocessing

--- plotCases.py
+++ plotCases.py
@@ -1,4 +1,3 @@
-import getXY
 import numpy as np
 import pandas as pd
 
@@ -6,7 +5,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-path = 'finiteBeam/'
+path = './finiteBeam/'
 
 # scaling for diffuse mode
 ## finiteBeam
@@ -21,13 +20,21 @@ path = 'finiteBeam/'
 
 line = 1
 
+def getDataValues( caseFile, line=1 ):
+    df = pd.read_csv(caseFile, index_col=0, header=None)[line]
+    return df.values
+
 for i in range(1,36):
-    dfMacro = pd.DataFrame(index=np.linspace(0,1,101))
-    dfMacro['macro50*4'] = pd.Series(getXY.getDataMink(path+'mink/50case'+str(i)+'.csv', line)[:,1]*5, index=dfMacro.index)
-    dfMacro['meso100'] = pd.Series( getXY.getDataMcHardy(path+'mcHardyRK/100case'+str(i)+'.csv', line)[:,1], index=dfMacro.index )
-    dfMacro['MC'] = pd.Series(getXY.getDataLena(path+'lena/lena'+str(i)+'.csv', line)[:,1], index=dfMacro.index)
+    dfMacro = pd.DataFrame(index=np.arange(0,0.99,0.01))
+    dfMacro['meso_q15_N50'] = getDataValues(path+'mcHardyRK_d3q15/50case'+str(i)+'.csv', line)[:-2]
+    dfMacro['meso_q15_N100'] = getDataValues(path+'mcHardyRK_d3q15/100case'+str(i)+'.csv', line)[:-2]
+    #dfMacro['meso_q7_N100'] = getDataValues(path+'mcHardyRK_d3q7/100case'+str(i)+'.csv', line)[:-2]
+    dfMacro['meso_q15_N150'] = getDataValues(path+'mcHardyRK_d3q15/150case'+str(i)+'.csv', line)[:-2]
+    #dfMacro['meso_q15_N100'] = getDataValues(path+'mcHardyRK_d3q15/100case'+str(i)+'.csv', line)[:-2]
+    #dfMacro['meso_q27_N100'] = getDataValues(path+'mcHardyRK/100case'+str(i)+'.csv', line)[:-2]
+    dfMacro['MC'] = getDataValues(path+'lena/lena'+str(i)+'.csv', line)[:-2]
     dfMacro.plot()
     plt.yscale('log')
     plt.title('case'+str(i))
-    plt.savefig(path+'compare_'+str(line)+"_case"+str(i)+'.png')
+    plt.savefig(path+'meso_d3q15_grid_'+str(line)+"_case"+str(i)+'.png')
     plt.close()
