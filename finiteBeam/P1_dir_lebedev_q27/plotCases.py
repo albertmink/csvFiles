9d83a915af3749882a82f28dfca772a0a371cb6b
albert mink <albert.mink@kit.edu>
2019-10-08 10:56:58 +0200

    lebedev 27 data

--- finiteBeam/P1_dir_lebedev_q27/plotCases.py
+++ finiteBeam/P1_dir_lebedev_q27/plotCases.py
@@ -16,9 +16,10 @@ def getDataValues( caseFile, line=1 ):
 for i in range(1,36):
     dfMacro = pd.DataFrame(index=np.arange(0,0.99,0.01))
     dfMacro['p1_q7_lebedev'] = getDataValues('./../P1_dir_lebedev_q7/100case'+str(i)+'.csv', line)[:-2]
-    dfMacro['p1_q15_lebedev'] = getDataValues('100case'+str(i)+'.csv', line)[:-2]
+    dfMacro['p1_q15_lebedev'] = getDataValues('./../P1_dir_lebedev_q15/100case'+str(i)+'.csv', line)[:-2]
+    dfMacro['p1_q27_lebedev'] = getDataValues('100case'+str(i)+'.csv', line)[:-2]
     dfMacro['MC'] = getDataValues('../lena/lena'+str(i)+'.csv', line)[:-2]
     dfMacro.plot()
     plt.yscale('log')
-    plt.savefig('./q6_q14_case'+str(i)+'.png')
+    plt.savefig('./q6_q14_q26_case'+str(i)+'.png')
     plt.close()
